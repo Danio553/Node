3ad3fd9574b0cb4e6482a3482bd1134bf626dd2a
Danio553 <sudzio30@op.pl>
2021-09-13 15:26:18 +0200

    Poprawka

--- Express.js
+++ Express.js
@@ -17,13 +17,17 @@ console.log('The area of a square with a width of 4 is ' + square.area(4));
 console.log('First');
 console.log('Second');
 
+setTimeout(function() {
+    console.log('First');
+    }, 8080);
+ console.log('Second');
+
 app.all('/secret', function(req, res, next) {
     console.log('Accessing the secret section ...');
-    next(); // pass control to the next handler
+    next(); 
   });
 
 const wiki = require('./wiki.js');
-// ...
 app.use('/wiki', wiki);
 
   
