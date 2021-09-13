2ed5b924864d78222b23dcd8984b938366ab328e
Danio553 <sudzio30@op.pl>
2021-09-13 15:10:13 +0200

    Co się dzieje 2

--- Express.js
+++ Express.js
@@ -9,3 +9,6 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 });
+
+const square = require('./square'); // Here we require() the name of the file without the (optional) .js file extension
+console.log('The area of a square with a width of 4 is ' + square.area(4));
