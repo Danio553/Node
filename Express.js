10fb77f7f74b18ddc0ebf8a089690c35240f3889
Danio553 <sudzio30@op.pl>
2021-09-13 15:23:41 +0200

    Wiki itd.

--- Express.js
+++ Express.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const router = express.Router();
 const app = express();
 const port = 8080;
 
@@ -16,7 +17,13 @@ console.log('The area of a square with a width of 4 is ' + square.area(4));
 console.log('First');
 console.log('Second');
 
-setTimeout(function() {
-    console.log('First');
-    }, 8080);
- console.log('Second');
+app.all('/secret', function(req, res, next) {
+    console.log('Accessing the secret section ...');
+    next(); // pass control to the next handler
+  });
+
+const wiki = require('./wiki.js');
+// ...
+app.use('/wiki', wiki);
+
+  
