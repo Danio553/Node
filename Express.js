80bf398c792682f0c5d5aeb24688b4958cd50f81
Danio553 <sudzio30@op.pl>
2021-09-13 15:14:53 +0200

    CO się dzieje 3

--- Express.js
+++ Express.js
@@ -12,3 +12,11 @@ app.listen(port, () => {
 
 const square = require('./square'); // Here we require() the name of the file without the (optional) .js file extension
 console.log('The area of a square with a width of 4 is ' + square.area(4));
+
+console.log('First');
+console.log('Second');
+
+setTimeout(function() {
+    console.log('First');
+    }, 8080);
+ console.log('Second');
